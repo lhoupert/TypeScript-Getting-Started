5f17e05ce204c1f3405fa3e1d2888e624997ca2f
lhoupert <10154151+lhoupert@users.noreply.github.com>
2022-12-13 12:04:56 +0000

    test various compiler options for type checking

--- app/app.ts
+++ app/app.ts
@@ -1,7 +1,15 @@
 function startGame(){
     // starting a new game
+
+    let playerName: string = 'Audrey';
+    logPlayer(playerName)
+    
     var messagesElement = document.getElementById('messages');
     messagesElement!.innerText = 'Welcome to MultiMath! Starting new game ...';
+}
+
+function logPlayer(name : string):  void{
+    console.log(`New game starting for player: ${name}`)
 }
 
 document.getElementById('startGame')!.addEventListener('click',startGame)
